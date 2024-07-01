16e5f19fd95898185e26c52b2c05c235daa53a17
Alex-1-1-7 <ce23002@ues.edu.sv>
2024-06-30 21:41:53 -0600

    version001

---
+++ src/Ejercicio001.java
@@ -0,0 +1,43 @@
+/*Busque cuantas veces se encuentra la palabra “Dios” en la biblia. */
+/*
+* Miguel Alexander Castro Escobar CE23002
+* */
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Ejercicio001 {
+    public static void main(String[] args) throws IOException {
+        String ruta = "archivos/BIBLIA_COMPLETA.txt";
+        String palabra = "Dios";
+        int contador = 0;
+
+        try {
+            File archivo = new File(ruta);
+            FileReader lector = new FileReader(archivo);
+            BufferedReader reader = new BufferedReader(lector);
+
+            String linea;
+            while ((linea = reader.readLine()) != null) {
+                String[] palabras = linea.split("\\s+"); 
+
+                for (String pal : palabras) {
+                    if (pal.toLowerCase().equals(palabra.toLowerCase())) {
+                        contador++;
+                    }
+                }
+            }
+
+            reader.close();
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("\nLa palabra "+palabra+" se encuenta "+contador+" veces.\n");
+
+    }
+}
